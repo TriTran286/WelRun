2f38d9a05853594f76a522a1c13ea33e381d0412
TriMinhTrann <111098294+TriMinhTrann@users.noreply.github.com>
2025-01-18 16:52:02 +0700

    update url

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss' // Import tailwindcss
+import tailwindcss from 'tailwindcss'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/WelRun/', // Add this line - must match your repository name
   plugins: [react()],
   css: {
     postcss: {
-      plugins: [tailwindcss()], // Use the imported tailwindcss here
+      plugins: [tailwindcss()],
     },
   },
 })
-
